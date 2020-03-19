4c3eefa64c587bcf9f63e4b75346ba903eb61569
hnifmaghfur <hnifmaghfur@gmail.com>
2020-03-19 14:56:08 +0700

    Perbaikan code pada form_upload

--- upload_form/upload.js
+++ upload_form/upload.js
@@ -7,17 +7,29 @@ http.createServer( function ( req, res ){
     if ( req.url === "/upload/" && req.method === "GET" ){
         fs.readFile( "upload.html", function ( err, data ){
             res.writeHead( 200, {'Content-Text' : 'text/html'});
-            if( err ){ throw err }
-
+            if( err ){ throw err; }
             res.end(data);
-
-            re
         });
     }
 
     if( req.url === "/upload/" && req.method === "POST" ){
         //mengambil data upload nya
         var getUpload = new formidable.IncomingForm();
+        getUpload.parse( req, function ( err, fields, files ){
+            //bikin direktori untuk menyimpan file yang di upload
+            var oldPath = files.filetoupload.path;
+            var newPath = __dirname + "aset/" + files.filetoupload.name;
+            
+            //memindahkan file yang di upload
+            mv( oldPath, newPath, function( err ){
+                if( err ) { throw err; }
+            //notif jika berhasil upload
+            console.log( 'File sukses di Upload.');
+            return res.end( "File sukses di Upload." );
+            });
+        });
     }
 
-});
+}).listen(8000);
+
+console.log("server listening on http://localhost:8000");
